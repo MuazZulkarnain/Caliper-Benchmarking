e664a73c74883361195da8c3807152c73cdd5202
Muaz Zul <idmuazzulkarnain@gmail.com>
2024-04-18 05:03:25 +0800

    optimized readAssetNew, simplified the operation

--- workload/readAssetNew.js
+++ workload/readAssetNew.js
@@ -4,11 +4,22 @@ const { WorkloadModuleBase } = require("@hyperledger/caliper-core");
 
 class MyWorkload extends WorkloadModuleBase {
   async submitTransaction() {
-    let txArgs = {
-      // TX arguments for "mycontract"
+    const randomId = Math.floor(Math.random() * this.roundArguments.assets + 1);
+    const assetName = `Powerplant ${randomId}`;
+    console.log(`Worker ${this.workerIndex}: Reading asset ${assetName}`);
+
+    // Use the correct contractFunction and contractArguments for reading assets
+    const readAssetRequest = {
+      contractId: "IoT-cc",
+      contractFunction: "readAsset",
+      invokerIdentity: "User1",
+      contractArguments: [
+        `{"key": {"@assetType":"project","project":"${assetName}"}}`,
+      ],
+      readOnly: true,
     };
 
-    return this.sutAdapter.invokeSmartContract("mycontract", "v1", txArgs, 30);
+    await this.sutAdapter.sendRequests(readAssetRequest);
   }
 }
 
